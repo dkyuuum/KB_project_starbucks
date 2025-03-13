5576065079e8a73d22c60793e6b81f9eaced3904
dkyuuum <dkyuuum@gmail.com>
2025-03-13 15:05:10 +0900

    [Fix] CORS 문제 해결

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
+//CORS문제 해결
+const cors = require('cors');
+app.use(cors());
+
+/*요청ContentType이 x-www-form-urlencoded인 경우
+요청의 body사용하고 싶다면 아래 함수를 사용하세요*/
+app.use(express.urlencoded({ extended: true }));
+
+/*요청ContentType이 application/json인 경우
+요청의 body사용하고 싶다면 아래 함수를 사용하세요*/
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('welcome!');
 });
