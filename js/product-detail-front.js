bef69b1e364fe53ddfa51d0b4b11a63aad94ea7a
dkyuuum <dkyuuum@gmail.com>
2025-03-14 17:37:41 +0900

    [Feat] 상품 상세 정보 연결 중...

---
+++ js/product-detail-front.js
@@ -0,0 +1,15 @@
+const init = async () => {
+  const URL = 'http://localhost:3000/product/detail/:prodNo';
+  let response = await getFetch(URL);
+};
+
+/**
+ * GET 요청
+ * */
+const getFetch = async (url) => {
+  return await fetch(url)
+    .then((response) => response.json()) // fetch 함수가 끝날 때까지 기다려라
+    .catch((err) => console.error(err));
+};
+
+init();
